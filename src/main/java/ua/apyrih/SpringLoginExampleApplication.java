70c11ab0346b8a0c3d453757d8bb1ada02f260ca
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-17 22:55:19 +0200

    Initial commit

---
+++ src/main/java/ua/apyrih/SpringLoginExampleApplication.java
@@ -0,0 +1,13 @@
+package ua.apyrih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLoginExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/apyrih/SpringLoginExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.apyrih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
