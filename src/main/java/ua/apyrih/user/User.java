c98636c7dc1461f81e9942c7d0f582cc4abf6168
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-17 23:34:10 +0200

    1. added User.java and UserRole.java

---
+++ src/main/java/ua/apyrih/user/User.java
@@ -0,0 +1,70 @@
+package ua.apyrih.user;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class User implements UserDetails {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private UserRole role;
+    private Boolean locked;
+    private Boolean enabled;
+
+    public User(String name, String username, String email, String password, UserRole role, Boolean locked, Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; // ...
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true; // ...
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ src/main/java/ua/apyrih/user/UserRole.java
@@ -0,0 +1,6 @@
+package ua.apyrih.user;
+
+public enum UserRole {
+    USER,
+    ADMIN
+}
