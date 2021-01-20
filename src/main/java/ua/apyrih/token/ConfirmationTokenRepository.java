92411e7e0dbcfa6794b683c108d3c5be705c59a7
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-20 14:35:47 +0200

    added ConfirmationTokenRepository.java

---
+++ src/main/java/ua/apyrih/token/ConfirmationTokenRepository.java
@@ -0,0 +1,8 @@
+package ua.apyrih.token;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+}
