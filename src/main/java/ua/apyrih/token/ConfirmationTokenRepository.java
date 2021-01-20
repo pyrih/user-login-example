a02f9dc96c1f8c4a9cd84684b5dd0985c4c16598
Andrii Pyrih <andrii.pyrih@gmail.com>
2021-01-20 14:37:47 +0200

    added findByToken method to ConfirmationTokenRepository

--- src/main/java/ua/apyrih/token/ConfirmationTokenRepository.java
+++ src/main/java/ua/apyrih/token/ConfirmationTokenRepository.java
@@ -3,6 +3,10 @@ package ua.apyrih.token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
 }
